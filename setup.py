d72360ee9e1ab5ac295c727f4696f442406f30f7
Benjamin Zores <benjamin.zores@gmail.com>
2025-06-11 10:12:59 +0200

    add python build and release files

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Copyright (c) The Kowabunga Project
+# Apache License, Version 2.0 (see LICENSE or https://www.apache.org/licenses/LICENSE-2.0.txt)
+# SPDX-License-Identifier: Apache-2.0
+
+import setuptools
+
+setuptools.setup(
+    setup_requires=['pbr', 'setuptools'],
+    pbr=True,
+    py_modules=[])
